fb0fc0a457bafab202e832956c6d11890967282d
ryancarolina <ryancarolina88@gmail.com>
2023-10-13 19:11:48 -0400

    UI and saving for skills tab

--- CharacterManager.py
+++ CharacterManager.py
@@ -3,6 +3,15 @@ from tkinter import ttk
 import tkinter.messagebox as messagebox
 
 class CharacterManager:
+    skills_labels_text = [
+            "Acrobatics (Dex)", "Animal Handling (Wis)", "Arcana (Int)", 
+            "Athletics (Str)", "Deception (Cha)", "History (Int)", 
+            "Insight (Wis)", "Intimidation (Cha)", "Investigation (Int)", 
+            "Medicine (Wis)", "Nature (Int)", "Perception (Wis)", 
+            "Performance (Cha)", "Persuasion (Cha)", "Religion (Int)", 
+            "Sleight of Hand (Dex)", "Stealth (Dex)", "Survival (Wis)"
+        ]
+    
     def __init__(self):
         self.character_data = {}
 
@@ -27,6 +36,7 @@ class CharacterManager:
             'Intelligence': entries[9].get(),
             'Wisdom': entries[10].get(),
             'Charisma': entries[11].get(),
+            'Skills': {skill_label: entry.get() for skill_label, entry in zip(self.skills_labels_text, self.skills_entries)},
             'Notes': notes_text.get("1.0", tk.END).strip()
         }
         self.character_data[player_index] = char_details
@@ -52,15 +62,28 @@ class CharacterManager:
         skills_frame = ttk.Frame(notebook)
         notebook.add(skills_frame, text="Skills")
 
+        self.skills_entries = []  # List to hold Entry widgets for skills
+
+        for i, text in enumerate(self.skills_labels_text):
+            tk.Label(skills_frame, text=f"{text}:").grid(row=i, column=0, sticky='e')
+            entry = tk.Entry(skills_frame)
+            entry.grid(row=i, column=1)
+            self.skills_entries.append(entry)  # Keep track of entry fields for later use
+
         notes_text = tk.Text(character_sheet_window, width=40, height=10)
         notes_text.pack()
-
+        # If character data exists for this player, populate the fields
         if player_index in self.character_data:
             for i, key in enumerate(core_stats_labels_text):
                 entries[i].insert(0, self.character_data[player_index].get(key, ''))
             saved_notes = self.character_data[player_index].get('Notes', '')
             notes_text.delete("1.0", tk.END)
             notes_text.insert("1.0", saved_notes)
+            
+            # Populate skill fields
+            skills_data = self.character_data[player_index].get('Skills', {})
+            for i, skill_label in enumerate(self.skills_labels_text):
+                self.skills_entries[i].insert(0, skills_data.get(skill_label, ''))
 
         save_button = tk.Button(character_sheet_window, text="Save", command=lambda: self.save_character(player_index, entries, notes_text) if self.validate_entries(entries) else messagebox.showerror("Invalid Input", "Please enter valid numbers for Level, AC, HP, and ability scores."))
         save_button.pack()
