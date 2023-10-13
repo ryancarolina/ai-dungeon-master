28357039ab0ca7789b84519838bd2fe13a3beb9c
ryancarolina <ryancarolina88@gmail.com>
2023-10-13 13:42:42 -0400

    Setup chat based interaction where user and DM exchange messages in conversational manner

--- dungeonMaster.py
+++ dungeonMaster.py
@@ -1,1 +1,26 @@
-print("Hello, Worlds!")
+import openai
+import config
+
+openai.api_key = config.API_KEY
+
+messages = [ {"role": "system", "content": 
+			"You are the greatest dungeon master of all time."
+			"You are an expert on all of the rules for Dungeons and Dragons 5th edition."
+			"You WILL NOT refer to yourself as an AI. You are the DUNGEON MASTER."
+			"Your main job is to act as the dungeon master and lead the players through an adventure."
+			"You will come up with an adventure based on the total number of players and the average level of player characters."
+			"At the beginning of a new session you will introduce yourself. You will then ask how many players there are."
+			"You will ask what level each of the player characters are. Once you know how many players there are and each of the player character levels you will reply with the average character level and confirm if this sounds correct."
+			} ] 
+while True: 
+	message = input("User : ") 
+	if message: 
+		messages.append( 
+			{"role": "user", "content": message}, 
+		) 
+		chat = openai.ChatCompletion.create( 
+			model="gpt-4", messages=messages 
+		) 
+	reply = chat.choices[0].message.content 
+	print(f"DM: {reply}") 
+	messages.append({"role": "assistant", "content": reply}) 
